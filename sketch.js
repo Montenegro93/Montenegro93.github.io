691425f4d79c0309fc8397ae9f450512d69ef3df
Montenegro93 <43181133+Montenegro93@users.noreply.github.com>
2020-04-30 12:19:46 -0400

    change site content

--- sketch.js
+++ sketch.js
@@ -1,73 +1,53 @@
-let scl = 60; //SLIDER?
-let inc = 0.01; // BETWEEN 0.01 & 0.1
-let numOfP = 2000;
-let rows,cols;
-let fr;
-let particules = [];
-let flowfield = [];
-let zoff = 0;
+let img1 , img2; // Declare variable 'img'.
+let flip = true;
+let changeSize = false;
 
 
+function preload(){
 
-function setup() {
-
-  createCanvas(windowWidth,windowHeight);
-
-
-  cols = floor(width / scl);
-  rows = floor(height / scl);
-
-
-
-flowfield = new Array(cols * rows);
-
-    //PARTICULES SPAWNER
-    for( let i = 0 ; i < numOfP   ; i++  ){
-      let p = new Particules(random(width),random(height));
-      particules.push(p);
-    }
+  img1 = loadImage('tsne_all_v5.png'); // Load the image
+  img2 = loadImage('tsne_grid_all_v5.jpg'); // Load the image
 
 }
 
+function setup() {
+  createCanvas(3000, 3000);
+  frameRate(24)
+}
+
 function draw() {
-  background(255);
+  // Displays the image at its actual size at point (0,0)
 
+background(0);
 
-  let yoff = 0;
-  for( let y = 0 ; y < rows ; y++ ) {
-    let xoff = 0;
-
-    for( let x = 0 ; x < cols ; x ++ ) {
-
-      let index = x + y * cols;
-      let angle = noise(xoff,yoff,zoff) * TWO_PI * 8 ;
-      let v = p5.Vector.fromAngle(angle);
-      v.setMag(0.2);
-
-      flowfield[index] = v;
-
-      xoff += inc;
-//DRAW VECTOR
-      // push();
-      // stroke(0,50);
-      //   translate(x*scl,y*scl);
-      //   rotate(v.heading());
-      //   line(0,0,scl,0);
-      // pop();
-
-    }
-    yoff += inc;
-    zoff += 0.003; //BETWEEN 0.0001 & 0.001
-  }
-
-  //FUNCTION APPLIED TO Particules
-  for(let p of particules){
-    p.follow(flowfield);
-    p.update();
-    p.warparound();
-    p.display();
-
+  if(changeSize == true){
+    size = 3000;
+    resizeCanvas(size,size);
+  }else{
+    size = 600;
+    resizeCanvas(size,size);
 
   }
 
+  if(flip == true ){
+    image(img1, 0, 0,size,size);
+  }else{
+    image(img2, 0, 0,size,size);
+
+  }
+ 
+}
+
+function keyPressed(){
+
+
+
+  if(key == 'c'){
+    changeSize = !changeSize
+  }
+
+  if(key =='x'){
+    flip = !flip;
+
+  }
 }
